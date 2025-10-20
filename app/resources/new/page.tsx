e510d44f33eaef9205ae4ed6a03a0004cc794c75
Wooloo <ayan0312000@gmail.com>
2025-10-20 21:36:16 +0800

    remove resource pages

--- app/resources/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NewResourceForm } from "@/components/resources/new-resource-form"
-
-export default function NewResourcePage() {
-	return (
-		<div className="container mx-auto px-4 py-8">
-			<NewResourceForm />
-		</div>
-	)
-}
